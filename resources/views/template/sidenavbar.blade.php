628561840bae2ab8ebbed40af8e61b094deea099
Toni Simanjuntak <58927171+tonisimanjuntak@users.noreply.github.com>
2025-09-22 21:54:35 +0700

    add kalbar transparant

--- resources/views/template/sidenavbar.blade.php
+++ resources/views/template/sidenavbar.blade.php
@@ -3,8 +3,9 @@
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ url('dashboard') }}">
-        <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
+        <div class="sidebar-brand-icon">
+
+            <img src="{{ asset('images/kalbar_transparant.png') }}" alt="" style="width: 30px;">
         </div>
         <div class="sidebar-brand-text mx-3">SIPEKA</div>
     </a>
